a7afa090b45b82078b01cdb0879c3e8c70aec8e7
rick2785 <rjhrabowskie@gmail.com>
2016-07-19 09:48:26 -0500

    Tinder Clone

---
+++ Tinder/Resources/ParseStarterProject-Swift-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  ParseStarterProject-Swift-Bridging-Header.h
+//  ParseStarterProject-Swift
+//
+//  Created by Rickey Hrabowskie on 6/25/16.
+//  Copyright © 2016 Parse. All rights reserved.
+//
+
+#ifndef ParseStarterProject_Swift_Bridging_Header_h
+#define ParseStarterProject_Swift_Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <ParseFacebookUtilsV4/PFFacebookUtils.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif /* ParseStarterProject_Swift_Bridging_Header_h */

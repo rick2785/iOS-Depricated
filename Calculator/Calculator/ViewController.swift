3debeaa6dbd356606f33fd03776f141a162dd54f
rick2785 <rjhrabowskie@gmail.com>
2016-07-29 18:34:08 -0500

    Calculator App

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Rickey Hrabowskie on 7/27/16.
+//  Copyright © 2016 Rickey Hrabowskie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet private var display: UILabel!
+    
+    private var userIsInTheMiddleOfTyping = false
+    
+    @IBAction private func touchDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
+    }
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    var savedProgram: CalculatorBrain.PropertyList?
+    
+    @IBAction func save() {
+        savedProgram = brain.program
+    }
+    
+    @IBAction func restore() {
+        if savedProgram != nil {
+            brain.program = savedProgram!
+            displayValue = brain.result
+        }
+    }
+    
+    private var brain = CalculatorBrain()
+    
+    @IBAction private func performOperation(sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        if let mathematicalSymbol = sender.currentTitle {
+            brain.performOperation(mathematicalSymbol)
+        }
+        displayValue = brain.result
+    }
+}
+
+        
+            //if mathematicalSymbol == "π" {
+            //    displayValue = M_PI
+            //} else if mathematicalSymbol == "√" {
+            //    displayValue = sqrt(displayValue)
+            //}
+
